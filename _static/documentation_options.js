9b0963317f9eccd7a83395c00967fee175446f69
oneflyingfish <a_flying_fish@outlook.com>
2021-09-19 16:24:07 +0000

    upload

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.dev1709+gfb306e499',
+    VERSION: '0.8.dev1719+g83c3846d0',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
