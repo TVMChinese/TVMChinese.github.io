3e56d7a14a0b7764e4957a179a580f7f7dc30f3c
oneflyingfish <a_flying_fish@outlook.com>
2021-10-03 13:20:20 +0000

    update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.dev1726+g998e0b3a6',
+    VERSION: '0.8.dev1727+ga882350ea',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
