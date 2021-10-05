15295f070010aad30610ee8a90928009014f5b06
oneflyingfish <a_flying_fish@outlook.com>
2021-10-05 10:39:58 +0000

    updata

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.dev1727+ga882350ea',
+    VERSION: '0.8.dev1728+g18870dfb0',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
