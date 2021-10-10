7a198de559426b7dd29a6e0f21779ec1eda754d4
oneflyingfish <a_flying_fish@outlook.com>
2021-10-10 04:37:40 +0000

    auto upload

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.dev1728+g18870dfb0',
+    VERSION: '0.8.dev1729+g7061bd00f',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
