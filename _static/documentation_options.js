ebe3570008989693bebe5899d89fa505f77d89b0
oneflyingfish <a_flying_fish@outlook.com>
2021-11-01 12:13:20 +0000

    auto upload

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.dev1949+g5082e8986',
+    VERSION: '0.8.dev1950+gd69b65423',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
